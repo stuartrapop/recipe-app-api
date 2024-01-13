202dfa65be4e9beae067567a6de2df6b90cde8a8
Stuart Rapoport <srapoport@dial-once.com>
2024-01-13 16:41:26 +0100

    wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,12 @@
 Calculator Functions
 """
 
+
 def add(x, y):
-	"""Add two numbers together"""
-	return x + y
+    """Add two numbers together"""
+    return x + y
+
 
 def subtract(x, y):
-	"""Subtract y from x and return value"""
-	return x - y
-
+    """Subtract y from x and return value"""
+    return x - y
