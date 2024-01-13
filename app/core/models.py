2af154172dd7e900edfb20f5806eeb9620b14dfa
Stuart Rapoport <srapoport@dial-once.com>
2024-01-13 17:58:15 +0100

    test email normalization

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """ Manager for user profiles """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserManager()
+
+    USERNAME_FIELD = 'email'
